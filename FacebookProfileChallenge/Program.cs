d922bb0770b7c25604ba60d4738d877ce99052f2
Anna Heselbarth <annaheselbarth@gmail.com>
2021-09-28 22:19:26 -0400

    bronze commits

--- FacebookProfileChallenge/Program.cs
+++ FacebookProfileChallenge/Program.cs
@@ -10,6 +10,20 @@ namespace FacebookProfileChallenge
     {
         static void Main(string[] args)
         {
+            Workplace firstWorkplace = new Workplace();
+
+            firstWorkplace.current = "ElevenFifty";
+            firstWorkplace.past = "HealthCoach";
+
+            Bio firstBio = new Bio();
+
+            firstBio.current = "Mom of 3 boys. Learning to Code and loving life!";
+
+            Person firstPerson = new Person("Anna", "Heselbarth", new DateTime(1982, 03, 21), firstWorkplace, firstBio);
+
+            Console.WriteLine(firstPerson.FullName);
+            Console.WriteLine(firstPerson.AgeInYears);
+            Console.ReadKey();
         }
     }
 }
---
+++ FacebookProfileLibrary/Profile.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacebookProfileLibrary
+{
+    public class Profile
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string FullName
+        {
+            get { return $"{FirstName} {LastName}"; }
+        }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public int AgeInYears
+        {
+            get
+            {
+                TimeSpan ageSpan = DateTime.Now - DateOfBirth;
+                double totalAgeInYears = ageSpan.TotalDays / 365.25;
+                return Convert.ToInt32(Math.Floor(totalAgeInYears));
+            }
+        
+            
+        }
+
+        public string Workplace { get; set; }
+
+        public string Bio { get; set; }
+
+        public Profile(string firstName, string lastName, DateTime dateOfBirth, string Workplace, string Bio)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            DateOfBirth = dateOfBirth;
+            Workplace = Workplace;
+            Bio = Bio;
+
+        }
+    }
+}
